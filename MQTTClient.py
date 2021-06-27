149d0e1b287dadc669c19f8909d6eae4ff6bea55
camillabarreto <camillabarretodesousa@gmail.com>
2021-06-27 11:43:41 -0300

    adicionado escopo da interface de mensagens (Message) e do tratador de envio de mensagens MQTT (MQTTClient)

---
+++ MQTTClient.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+from Message import Message
+
+class MQTTClient(Message):
+
+    def __init__(self):
+        # host
+        # port
+        # keepalive
+        # bind_address
+        # cliente paho
+        # topic
+        pass
+
+    def send():
+        pass
---
+++ Message.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''Interface para troca de mensagens'''
+class Message():
+
+    def __init__(self):
+        pass
+
+    def send():
+        '''Envia as mensagens para o intermediário da comunicação'''
+        pass
