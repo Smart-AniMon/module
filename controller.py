ec76c861cfc038f814e25acfa9b66d64d1b6b2eb
camillabarreto <camillabarretodesousa@gmail.com>
2021-07-12 15:23:24 -0300

    fotografando e enviando mensagem generica ao broker

--- controller.py
+++ controller.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
+from MQTTClient import MQTTClient
 from threading import Timer
 from Camera import Camera
 
+
 class Controller():
 
-    def __init__(self, t_period, id_camera):
+    def __init__(self, t_period, id_camera, messenger):
         self.t = t_period
         self.timer = None
         self.count = 0
         self.id_camera = id_camera
+        self.messenger = messenger
 
     def run(self):
         self.timer = Timer(self.t, self.identify)
@@ -18,11 +21,31 @@ class Controller():
     def identify(self):
         caminho = Camera.get_value(self.id_camera, "./images/image"+str(self.count)+".png")
         print(str(self.count)+" - PHOTOGRAPH: "+caminho)
+        self.messenger.send("broker "+str(self.count))
+        
+        # 1. transformar imagem em bytes
+        # 2. adicionar num objeto json
+        # 3. enviar para o broker
+
         self.count += 1
         self.run()
 
+def get_credentials():
+    ref_arquivo = open("credentials.txt","r") 
+    return ref_arquivo.readlines()
 
 if __name__ == "__main__":
-    controller = Controller(1, 0) # t_period (mudar para 60 segundos), id_camera (0: camNotebook, 2: camUSB)
+    
+    credentials = get_credentials()
+
+    username = credentials[0].rstrip('\n')
+    password = credentials[1].rstrip('\n')
+    hostname = credentials[2].rstrip('\n')
+    topic = credentials[3].rstrip('\n')
+    
+    mqtt = MQTTClient(hostname, username, password, topic)
+
+    controller = Controller(1, 0, mqtt) # t_period (mudar para 60 segundos), id_camera (0: camNotebook, 2: camUSB)
+    
     controller.run()
     print("Run")
